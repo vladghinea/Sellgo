82132bd5a849bdf80dc9956e44428ceace688660
apetreibogdan <70704260+apetreibogdan@users.noreply.github.com>
2021-12-15 14:15:53 +0200

    Client Controler Initiated

---
+++ El Proyecte Grande/Controllers/ClientController.cs
@@ -0,0 +1,29 @@
+﻿using El_Proyecte_Grande.Models;
+using El_Proyecte_Grande.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace El_Proyecte_Grande.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        public ServiceClient seviceClient;
+
+        public ClientController(ServiceClient seviceClient)
+        {
+            this.seviceClient = seviceClient;
+        }
+
+
+        //GET Clients
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public List<Client> GetClients()
+        {
+            return seviceClient.GetClientsList();
+        }
+    }
+}
---
+++ El Proyecte Grande/Services/ServiceClient.cs
@@ -0,0 +1,28 @@
+﻿using El_Proyecte_Grande.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace El_Proyecte_Grande.Services
+{
+    public class ServiceClient
+    {
+       
+        private readonly AppDbContext _db;
+        public ServiceClient([FromServices] AppDbContext db)
+        {
+               _db = db;
+        }
+
+       public List<Client> GetClientsList()
+        {
+
+            return _db. .Clients.Select(client=>client).ToList();
+        }
+
+
+
+        
+        
+    }
+}
