debed06f2b04dac10f713e838965424055fec16c
vladghinea <79163522+vladghinea@users.noreply.github.com>
2021-12-15 17:47:08 +0200

    Add User Controller and User Service

---
+++ El Proyecte Grande/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using El_Proyecte_Grande.Models;
+using El_Proyecte_Grande.Repository;
+using El_Proyecte_Grande.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace El_Proyecte_Grande.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IAppDbRepository _db;
+        private ServiceUser serviceUser;
+
+        public UserController(IAppDbRepository db)
+        {
+            _db = db;
+            serviceUser = new ServiceUser(_db);
+        }
+
+        [HttpGet]
+        public async Task<List<User>> GetUsers()
+        {
+            return await serviceUser.GetUsers();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<User> GetUser([FromRoute] int id)
+        {
+            return await serviceUser.GetUser(id);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (serviceUser.TryAddUser(user))
+            {
+                serviceUser.AddUser(user);
+                return Ok(user);
+            }
+            return BadRequest();
+
+        }
+    }
+}
---
+++ El Proyecte Grande/Services/ServiceUser.cs
@@ -0,0 +1,44 @@
+﻿using El_Proyecte_Grande.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using El_Proyecte_Grande.Utils;
+using El_Proyecte_Grande.Repository;
+
+namespace El_Proyecte_Grande.Services
+{
+    public class ServiceUser 
+    {
+        private IAppDbRepository _db;
+        public ServiceUser(IAppDbRepository db)
+        {
+            _db = db;
+        }
+        public async Task<User> GetUser(int id)
+        {
+            return await _db.Data.Users.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _db.Data.Users.ToListAsync();
+        }
+
+        public async void AddUser(User user)
+        {
+            user.Password = Utils.Helper.HashPassword(user.Password);
+            await _db.Data.SaveChangesAsync();
+        }
+        public bool TryAddUser(User user)
+        {
+            if (user.Password == user.ConfirmPassword)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
