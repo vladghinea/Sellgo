c9fc36dfabcc48a9b36b369e29d9be8b07dd3bbc
apetreibogdan <70704260+apetreibogdan@users.noreply.github.com>
2022-01-21 17:20:51 +0200

    Add Appexcharts to Dashboard

--- sellassistent/src/pages/Dashboard.jsx
+++ sellassistent/src/pages/Dashboard.jsx
@@ -1,9 +1,42 @@
-import Board from "../components/inDashboardLayout/Bord"
+import Chart from 'react-apexcharts'
 
 import StatusCard from "../components/status-card/StatusCard"
 
 import statusCard from '../assets/JsonData/status-card-data.json'
 
+import { Link } from 'react-router-dom'
+
+
+const chartOptions = {
+    series: [{
+        name: 'Online Customers',
+        data: [40,70,20,90,36,80,30,91,60]
+    }, {
+        name: 'Store Customers',
+        data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
+    }],
+    options: {
+        color: ['#6ab04c', '#2980b9'],
+        chart: {
+            background: 'transparent'
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+        },
+        legend: {
+            position: 'top'
+        },
+        grid: {
+            show: false
+        }
+    }
+}
 
 const Dashboard = () => {
     return (
@@ -25,7 +58,25 @@ const Dashboard = () => {
                     </div>
                 </div>
                 <div className="col-6">
-                    <div className="card full-height"></div>
+                    <div className="card full-height">
+                        <Chart
+                        options={chartOptions}
+                        series={chartOptions.series}
+                        type='line'
+                        height='100%'
+                        />                         
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__header">
+                        <h3>top customes</h3>
+                    </div>
+                    <div className="card__body">
+                        {/*table*/}
+                    </div>
+                    <div className="card__footer">
+                        <Link to='/'>View All</Link>
+                    </div>
                 </div>
             </div>
         </div>
